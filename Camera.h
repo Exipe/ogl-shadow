16a211422d85eeccfa180b48e135cd06521765f1
Ludwig <ludwig.exipe@gmail.com>
2020-05-28 15:56:43 +0200

    moving camera

---
+++ Camera.h
@@ -0,0 +1,32 @@
+//
+// Created by Ludwig on 5/28/2020.
+//
+
+#ifndef OGL_SHADOW_CAMERA_H
+#define OGL_SHADOW_CAMERA_H
+
+#include <glm/glm.hpp>
+
+class Camera {
+private:
+    float yaw;
+    float pitch;
+
+    void updateRotation();
+
+public:
+    glm::vec3 cameraPos;
+    glm::vec3 cameraFront;
+    glm::vec3 cameraUp;
+
+    explicit Camera(glm::vec3 cameraPos, float yaw, float pitch);
+    glm::mat4 getViewMatrix();
+
+    void rotate(float x, float y);
+    void moveForward(float speed);
+    void moveBackward(float speed);
+    void moveRight(float speed);
+    void moveLeft(float speed);
+};
+
+#endif //OGL_SHADOW_CAMERA_H
